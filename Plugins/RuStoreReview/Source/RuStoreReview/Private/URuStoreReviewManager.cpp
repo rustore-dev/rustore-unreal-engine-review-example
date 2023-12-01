9aa92d6c9c63bda12e484abdc6f472c5e71bde1d
rpelmegov <rpelmegov@ya.ru>
2023-12-01 07:44:52 +0300

    Update plugin version

--- Plugins/RuStoreReview/Source/RuStoreReview/Private/URuStoreReviewManager.cpp
+++ Plugins/RuStoreReview/Source/RuStoreReview/Private/URuStoreReviewManager.cpp
@@ -6,7 +6,7 @@
 
 using namespace RuStoreSDK;
 
-const FString URuStoreReviewManager::PluginVersion = "0.4";
+const FString URuStoreReviewManager::PluginVersion = "1.0";
 URuStoreReviewManager* URuStoreReviewManager::_instance = nullptr;
 bool URuStoreReviewManager::_bIsInstanceInitialized = false;
 
